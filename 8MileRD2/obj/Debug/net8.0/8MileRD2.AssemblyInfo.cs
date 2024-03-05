f57fe2ecfe6af60d40e5f757447434241dc27dfd
David Obadovic <david.obadovic3@gmail.com>
2024-03-05 10:30:27 +0100

    NewUIForEdit,Create,Delete

--- 8MileRD2/obj/Debug/net8.0/8MileRD2.AssemblyInfo.cs
+++ 8MileRD2/obj/Debug/net8.0/8MileRD2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("8MileRD2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35f2690ae2782d0b7ff9a48a054ed69cbdd1bd24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bee4de618df8bf802ab63475c0c55212864a24fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("8MileRD2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("8MileRD2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
