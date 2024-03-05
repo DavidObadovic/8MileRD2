8ec8c1e1c6f7267675c3bdf1a3fc2963d2d708a2
David Obadovic <david.obadovic3@gmail.com>
2024-03-05 10:33:15 +0100

    NewUiForCategory

--- 8MileRD2/obj/Debug/net8.0/8MileRD2.AssemblyInfo.cs
+++ 8MileRD2/obj/Debug/net8.0/8MileRD2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("8MileRD2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bee4de618df8bf802ab63475c0c55212864a24fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f57fe2ecfe6af60d40e5f757447434241dc27dfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("8MileRD2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("8MileRD2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
